b27d2fa157fb6e2e06e98da9c0fc4c17ad53c9ab
Chris Doherty <chris@chrisdoherty.io>
2020-09-26 22:34:32 +0000

    commit aa443bc88d6acd99b42e33e0f33150c518028281 Author: Chris Doherty <chris.doherty@alteryx.com> Date:   Sat Sep 26 16:33:19 2020 -0600
        Add TIL on Go integer conversion

--- en.search.min.59f3f7c855616b659165db2f4610831c5fc9c50a5cdc1ea43bda95e8803f6238.js
+++ en.search.min.59f3f7c855616b659165db2f4610831c5fc9c50a5cdc1ea43bda95e8803f6238.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.e9dfba584f02aff6ce36ca0319fb4945cbfd227e161de6cf0e10a3a8431d5f37.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.4977be3d982131310c331dbbe3eafdbdbfdfb50996574d1faf947f7602cb6c9b.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}

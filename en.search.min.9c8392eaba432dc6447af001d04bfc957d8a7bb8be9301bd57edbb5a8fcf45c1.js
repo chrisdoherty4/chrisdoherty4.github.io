a457bbd7aae2ed34f6724d52bdb738bf160cf4ce
Chris Doherty <chris@chrisdoherty.io>
2020-09-26 23:09:05 +0000

    commit 4cccf2c0c213effd792e62bcb209ee8aa4aacc13 Author: Chris Doherty <chris.doherty@alteryx.com> Date:   Sat Sep 26 17:08:14 2020 -0600
        Undraft determining endianness in Go

--- en.search.min.9c8392eaba432dc6447af001d04bfc957d8a7bb8be9301bd57edbb5a8fcf45c1.js
+++ en.search.min.9c8392eaba432dc6447af001d04bfc957d8a7bb8be9301bd57edbb5a8fcf45c1.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.466415abbe6011db003ea92d9a7996b794db295fb37f7ec5ae6b6fc2cce7cea5.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.00f143e9697edce054132e0cb43cf86f74b9f64b31a35a0eb3a62a95e1cb05e5.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
